2cf5145bb5aeced92b2dc77470285ef57a876da5
Jeremiah Mendoza <jeremiah@tutorbloc.com>
2022-05-31 20:25:05 +0100

    Initial Commit

---
+++ AnimalExplorer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimalExplorer
+//
+//  Created by Jeremiah Mendoza on 31/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
